ea065366f4e0e1f734ef9ceef15936a92ca8a01c
lingluo <lingluo511@gmail.com>
2020-05-12 16:16:10 +0800

    增加java8支持

--- app/src/main/java/cn/leo/pagingktx/MainActivity.kt
+++ app/src/main/java/cn/leo/pagingktx/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //model.insert()
+        model.insert()
         initView()
     }
 
