23b1c69e6a5fca092985097974e49a45f62fe57e
lingluo <lingluo511@gmail.com>
2020-09-24 17:50:05 +0800

    [add] 修复adapter的position不准确问题

--- paging_ktx/src/main/java/cn/leo/paging_ktx/PagedListAdapterKtx.kt
+++ paging_ktx/src/main/java/cn/leo/paging_ktx/PagedListAdapterKtx.kt
@@ -172,7 +172,7 @@ abstract class PagedListAdapterKtx<T : Any> : PagingDataAdapter<T, RecyclerView.
         @get:LayoutRes
         var itemLayoutResId: Int = 0
         val position
-            get() = viewHolder.mPosition
+            get() = viewHolder.absoluteAdapterPosition
         val itemView: View = viewHolder.itemView
         val context: Context = itemView.context
         var tag: Any? = null
